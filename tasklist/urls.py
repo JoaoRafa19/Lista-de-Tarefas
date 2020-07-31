5bc92e18ce2b46d674f975d3cab6cd445bb91d57
JoaoRafa19 <joaopedrorafael19@gmail.com>
2020-07-31 15:13:18 -0300

    commit

--- tasklist/urls.py
+++ tasklist/urls.py
@@ -4,10 +4,8 @@ from tasks.views import index, adicionar_tarefa, update_tarefa, delete_tarefa
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
     path('', index, name='index'),
     path('add/', adicionar_tarefa, name='add'),
     path('update/<int:id>/', update_tarefa, name='update'),
     path('delete/<int:id>/', delete_tarefa, name='delete'),
-
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -5,10 +5,8 @@ from .forms import TarefaForm
 
 # Create your views here.
 def index(request):
-    
     data = {}
     data['tarefas'] = Tarefa.objects.all()
-
     return render(request, 'tasks/index.html', data)
 
 
@@ -18,23 +16,20 @@ def adicionar_tarefa(request):
     if form.is_valid():
         form.save()
         return redirect('index')
-
     data['form'] = form
     return render(request, 'tasks/form.html', data)
+
 
 def update_tarefa(request, id):
     tarefa = Tarefa.objects.get(id=id)
     form = TarefaForm(request.POST or None, instance=tarefa)
-
     if form.is_valid():
         form.save()
         return redirect('index')
-    
     return render(request, 'tasks/form.html', {'form':form, 'tarefa':tarefa})
 
+
 def delete_tarefa(request, id):
-    pass
     tarefa = Tarefa.objects.get(id=id)
     tarefa.delete()
-
     return redirect('index')
